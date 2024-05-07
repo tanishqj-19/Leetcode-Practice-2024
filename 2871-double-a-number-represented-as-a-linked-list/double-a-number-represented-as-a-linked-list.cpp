35ab1717bacb3c248407771ebd0f74e8dd1def36
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-05-07 09:56:24 +0530

    Time: 191 ms (33.94%) | Memory: 138.9 MB (11.61%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
@@ -27,23 +27,20 @@ public:
 
         string num2 = num1;
 
-        int n = num1.size();
-        int  i = n-1, j = n-1;
+        
+        int  i = num1.size()-1;
         int carry = 0;
         ListNode* newhead = new ListNode(0);
 
          temp = newhead;
-        while(i >= 0 || j >= 0){
+        while(i >= 0){
             int x = carry;
-            if(i >= 0){
-                x += num1[i] - '0';
-                i--;
-            }
-
-            if(j >= 0){
-                x += num2[j] - '0';
-                j--;
-            }
+            
+            x += num1[i] - '0';
+            x += num2[i] - '0';
+    
+            
+            i--;
 
             carry = x / 10;
             temp->next = new ListNode(x % 10);
