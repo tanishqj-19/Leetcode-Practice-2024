b325eae5050b610b14b222a2705401dde17afac0
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-05-27 13:52:12 +0530

    Time: 9 ms (85.30%) | Memory: 12.2 MB (91.27%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,57 +1,4 @@
-class DisjointSet{
-public:
-    vector<int> parent, size, rank;
 
-    DisjointSet(int n){
-        for(int i = 0; i<n; i++){
-           parent.push_back(i);
-           size.push_back(1); 
-           rank.push_back(0);
-        }
-    }
-
-
-    int find(int node){
-        if(parent[node] == node)
-            return node;
-        return parent[node] = find(parent[node]);
-    }
-
-    void unionSize(int u, int v){
-        int ulu = find(u), ulv = find(v);
-
-        if(ulu == ulv)
-            return;
-        if(size[ulu] >= size[ulv]){
-            parent[ulv] = ulu;
-            size[ulu] += size[ulv];
-        }
-        else{
-            parent[ulu] = ulv;
-            size[ulv] += size[ulu];
-
-        }
-
-        return;
-    }
-    void unionRank(int x, int y){
-        int upx = find(x), upy = find(y);
-
-        if(upx == upy){
-            
-            return;
-        }
-        if(rank[upx] > rank[upy]){
-            parent[upy] = upx;
-        }else if(rank[upx] < rank[upy]){
-            parent[upx] = upy;
-        }else{
-            parent[upx] = upy;
-            rank[upx]++;
-        }
-    }
-
-};
 
 bool isValid(int i, int j , int n){
     return i >= 0 && i < n && j >= 0 && j < n;
@@ -62,15 +9,15 @@ public:
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
 
-        DisjointSet ds(n *n);
-
         priority_queue< pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, greater<pair<int, pair<int,int>>> > pq;
+
         int ans = 0;
         pq.push({grid[0][0], {0,0}});
         bool vis[n][n];
         memset(vis, false, sizeof vis);
         vis[0][0] = true;
         vector<int> dir = {0, -1, 0, 1, 0};
+        
         while(!pq.empty()){
             int d = pq.top().first;
             int i = pq.top().second.first;
