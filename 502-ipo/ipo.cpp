05c6e044abc5930e55387385a30c4a8a5f34bff3
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-06-21 13:01:40 +0530

    Time: 221 ms (5.53%) | Memory: 101.2 MB (5.09%) - LeetSync

--- 502-ipo/ipo.cpp
+++ 502-ipo/ipo.cpp
@@ -1,11 +1,6 @@
 class Solution {
 
-    static bool cmp(vector<int> &a, vector<int> &b){
-
-        if(a[0] == b[0])
-            return a[1] > b[1];
-        return a[0] < b[0];
-    }
+    
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
         priority_queue<int>pq;
@@ -18,7 +13,7 @@ public:
             arr[i] = {capital[i], profits[i]};
         }
 
-        sort(arr.begin(), arr.end(), cmp);
+        sort(arr.begin(), arr.end());
         int i = 0 ;
         while(k -- > 0){
 
