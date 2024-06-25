ba9728d9e9362731d464f58d600efb9feb004f41
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-06-25 12:40:14 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (68.93%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -3,33 +3,18 @@ class Solution {
     
     const int MOD = 1e9 + 7;
 
-    long  solve(int n, int i, bool isGap, vector<vector<int>> &dp){
-        if(i > n)
-            return 0;
-        if(i == n)
-            return !isGap;
-        if(dp[i][isGap] != -1)
-            return dp[i][isGap];
-        
-        if(isGap){
-            return dp[i][isGap] = (solve(n, i+1, false, dp) % MOD + solve(n, i + 1, true, dp) % MOD) % MOD;
-        }
-
-        return dp[i][isGap] = (solve(n, i+1, false, dp) + solve(n, i+2, false, dp) + (2 * solve(n, i+2, true, dp)) % MOD) % MOD;
-        
-
-    }
+    
 public:
     int numTilings(int n) {
         
-        vector<vector<long long>> dp(n + 2, vector<long long>(2, 0));
+        vector<vector<long long>> dp(3, vector<long long>(2, 0));
 
         dp[1] = {1, 1}; dp[2] = {2, 2};
 
         for(int i = 3; i<=n; i++){
-            dp[i][0] = (dp[i-1][0] + dp[i-2][0] + 2 * dp[i-2][1]) % MOD;
-            dp[i][1] = (dp[i-1][0] + dp[i-1][1]) % MOD;
+            dp[i % 3][0] = (dp[(i-1) % 3][0] + dp[(i-2) % 3][0] + 2 * dp[(i - 2) % 3][1]) % MOD;
+            dp[i % 3][1] = (dp[(i-1) % 3][0] + dp[(i - 1) % 3][1]) % MOD;
         }
-        return dp[n][0];
+        return dp[n % 3][0];
     }
 };
