4b7de50f748e2de98e5087d82465acebdbbefb83
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-06-25 09:28:18 +0530

    Time: 5 ms (15.49%) | Memory: 10.1 MB (42.71%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,20 @@
+
+class Solution {
+    void solve(TreeNode* root, int &sum){
+        if(!root)
+            return;
+        solve(root->right, sum);
+        sum += root->val;
+        root->val = sum;
+        solve(root->left, sum);
+
+        return ;
+    }
+public:
+    TreeNode* bstToGst(TreeNode* root) {
+        int sum = 0;
+        solve(root, sum);
+
+        return root;
+    }
+};
