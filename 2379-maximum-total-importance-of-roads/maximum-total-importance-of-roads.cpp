9181b83d7e67b851444de63492581c7d820ee490
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-06-28 10:50:31 +0530

    Time: 251 ms (82.41%) | Memory: 117.6 MB (75.52%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
@@ -1,30 +1,20 @@
 class Solution {
 
-    static bool cmp(vector<int> &a, vector<int> &b){
-        return a.size() > b.size();
-    }
+    
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
         long long ans = 0;
 
-        vector<vector<int>> adj(n);
-        for(int i = 0; i<n; i++) adj[i].push_back(i);
+        vector<long long> deg(n, 0);
         for(auto &x: roads){
-            adj[x[0]].push_back(x[1]);
-            adj[x[1]].push_back(x[0]);
+            deg[x[0]]++;
+            deg[x[1]]++;
         }
 
-        sort(adj.begin(), adj.end(), cmp);
+        sort(deg.begin(), deg.end());
 
-        int y = n;
-        unordered_map<int, int> priority;
-        for(auto &x: adj){
-            priority[x[0]] = y;
-            y--;
-        }
-
-        for(auto &x: roads){
-            ans += (priority[x[0]] + priority[x[1]]);
+        for(int x = 0; x <n ;x++){
+            ans = ans + (long long) (deg[x] * (x + 1));
         }
 
         return ans;
