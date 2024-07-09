b56a93446cbf5838aa2ba70a612c9426e2a4f6b8
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-07-09 09:45:49 +0530

    Time: 147 ms (66.18%) | Memory: 76.5 MB (83.82%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -1,27 +1,13 @@
 class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
-        int n  = customers.size();
-
-        if(n == 1)
-            return customers[0][1] / 1.0;
-
-        double avg = customers[0][1] / 1.0;
-        int prevWait = customers[0][1] + customers[0][0];
-
-        for(int i = 1; i<n; i++){
-            if(prevWait >= customers[i][0]){
-                int curr = prevWait + customers[i][1] - customers[i][0];
-                avg += curr;
-                prevWait = (prevWait + customers[i][1]);
-            }else{
-                avg += customers[i][1];
-                prevWait = (customers[i][0] + customers[i][1]);
-            }
+        
+        int curr = 0, n = customers.size(); double avg = 0;
+        for(auto &x: customers){
+            curr = max(x[0], curr) + x[1];
+            avg += (curr - x[0]);
         }
 
         return (double) avg / n;
-
-        
     }
 };
