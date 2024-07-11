5d30bfc1c4747ea1f201ec523e419bccf55e2745
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-07-11 22:43:26 +0530

    Time: 139 ms (52.63%) | Memory: 62.8 MB (38.86%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,50 +1,42 @@
 class Solution {
-    vector<int> ans;
-
-    bool dfs(int node, vector<vector<int>>& graph, vector<bool> &vis, vector<bool> &pathVis ){
-        vis[node] = true;
-        pathVis[node] = true;
-
-
-        for(int nei: graph[node]){
-            if(!vis[nei]){
-                if(dfs(nei, graph, vis, pathVis))
-                    return true;
-            }else if(pathVis[nei]){
-                return true;
-            }
-        }
-
-
-
-
-        pathVis[node] = false;
-        return false;
-    }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        
-
-        
         int n = graph.size();
-        
-        
-        vector<bool> vis(n, false);
-        vector<bool> pathVis(n, false);
-
-        for(int i=0; i<n; i++){
-            if(!vis[i]){
-                dfs(i, graph, vis, pathVis);
-                    
-                
+        vector<int> adj[n];
+        queue<int> q;
+        vector<int> in(n, 0);
+        for(int i = 0; i<n; i++){
+            if(!graph[i].empty()){
+                for(auto &x: graph[i]){
+                    adj[x].push_back(i);
+                }
+            }else{
+                q.push(i);
             }
         }
 
-        for(int i=0; i<n; i++){
-            if(pathVis[i] == false && vis[i])
-                ans.push_back(i);
+        for(int i = 0; i<n; i++){
+            for(auto &x: adj[i]){
+                in[x]++;
+            }
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            int node = q.front(); q.pop();
+
+            ans.push_back(node);
+
+            for(auto &x: adj[node]){
+                in[x]--;
+                if(in[x] == 0)
+                    q.push(x);
+            }
         }
 
+        sort(ans.begin(), ans.end());
         return ans;
+        
+
+
     }
 };
