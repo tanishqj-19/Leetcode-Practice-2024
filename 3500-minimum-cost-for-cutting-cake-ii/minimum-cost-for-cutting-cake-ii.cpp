8b02737eda43c88bd1ff575ec949a366bccbd4b4
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-07-14 11:27:12 +0530

    Time: 326 ms (100.00%) | Memory: 233.7 MB (100.00%) - LeetSync

---
+++ 3500-minimum-cost-for-cutting-cake-ii/minimum-cost-for-cutting-cake-ii.cpp
@@ -0,0 +1,71 @@
+#define ll long long
+
+class Solution {
+public:
+    long long minimumCost(int n, int m, vector<int>& hcut, vector<int>& vcut) {
+        sort(hcut.begin(), hcut.end());
+        sort(vcut.begin(), vcut.end());
+        ll cost = 0;
+        
+        
+        int hi = 0, vi = 0;
+        ll mh = 1, mv = 1;
+
+        while(vcut.size() || hcut.size()){
+            ll vv = -1, hv = -1;
+
+            if(!vcut.empty()){
+                vv = vcut.back();
+            }
+
+            if(!hcut.empty()){
+                hv = hcut.back();
+            }
+
+            if(hv >= vv){
+                cost += (hv * mv);
+                mh++;
+                hcut.pop_back();
+            }else{
+                cost += (vv * mh);
+                vcut.pop_back();
+
+                mv++;
+                
+            }
+        }
+
+        return cost;
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+          
+       
+        
+        
+        
+       
+        return cost;
+    }
+};
