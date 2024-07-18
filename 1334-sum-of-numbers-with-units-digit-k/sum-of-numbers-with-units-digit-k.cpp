1c555564c3b6e559f0e22fca74f14171ac9129ae
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-07-19 00:28:24 +0530

    Time: 3 ms (66.46%) | Memory: 7.5 MB (49.84%) - LeetSync

--- 1334-sum-of-numbers-with-units-digit-k/sum-of-numbers-with-units-digit-k.cpp
+++ 1334-sum-of-numbers-with-units-digit-k/sum-of-numbers-with-units-digit-k.cpp
@@ -1,66 +1,16 @@
 class Solution {
-    vector<int> values;
-
-    // int solve(int i, int num, vector<vector<int>> &dp){
-        
-    //     if(i < 0)
-    //         return 1e9;
-    //     if(i == 0){
-    //         if(num % values[i] == 0)
-    //             return num / values[i];
-    //         return 1e9;
-    //     }
-    //     if(dp[i][num] != -1)
-    //         return dp[i][num];
-        
-    //     int notPick = solve(i-1, num, dp);
-
-    //     int pick = 1e9;
-    //     if(values[i] <= num){
-    //         pick = 1 + solve(i, num-values[i], dp);
-    //     }
-
-    //     return dp[i][num] =  min(pick, notPick);
-    // }
-
 public:
     int minimumNumbers(int num, int k) {
         
+
         if(num == 0)
             return 0;
-        for(int  i = 1 ; i<= num; i++){
-            if(i % 10 == k)
-                values.push_back(i); // in sorted order
-        }
-        if(values.empty())
-            return -1;
+        for(int i = 1; i<=10; i++){
 
-        int n = values.size();
-
-        vector<int> curr(num + 1, 1e9), prev(num + 1,  1e9);
-        for(int i=0; i<=num; i++){
-            if(i % values[0] == 0)
-                prev[i] = i / values[0];
+            if( ((i * k) % 10 == num % 10) && (i * k <= num))
+                return i;
         }
 
-        
-
-        for(int i = 1; i<n; i++){
-            for(int val = 0; val <= num; val++){
-                int notPick = prev[val], pick = 1e9;
-                if(values[i] <= val){
-                    pick = 1 + curr[val-values[i]];
-                }
-
-                curr[val] = min(pick, notPick);
-            }
-            prev = curr;
-        }
-
-        // int ans = solve(n-1, num, dp);
-
-        return prev[num] == 1e9? -1:  prev[num];
-
-
+        return -1;
     }
 };
