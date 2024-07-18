f600baad663d60069d837bc40db42c55bc8cc46f
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-07-19 00:33:29 +0530

    Time: 31 ms (89.87%) | Memory: 7.4 MB (36.27%) - LeetSync

--- 2541-sum-of-number-and-its-reverse/sum-of-number-and-its-reverse.cpp
+++ 2541-sum-of-number-and-its-reverse/sum-of-number-and-its-reverse.cpp
@@ -3,11 +3,16 @@ public:
     bool sumOfNumberAndReverse(int num) {
         if(num == 0)
             return true;
+        auto rev = [](int n) {
+            int rn = 0;
+            for (; n; n /= 10)
+                rn = rn * 10 + n % 10;
+            return rn;
+        };  
         
-        for(int i = num/2; i<num; i++){
-            string x = to_string(i);
-            reverse(x.begin(), x.end());
-            if(stoi(x) + i == num)
+        for(int i = num/2; i<=num; i++){
+            
+            if(rev(i) + i == num)
                 return true;
         }
 
