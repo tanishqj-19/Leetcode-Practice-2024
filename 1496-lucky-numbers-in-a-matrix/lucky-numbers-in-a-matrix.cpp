0502fb3c6918cfc8650ea3da78db275801365a84
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-07-19 15:38:26 +0530

    Time: 20 ms (23.27%) | Memory: 14.4 MB (18.75%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
@@ -8,12 +8,7 @@ public:
         for(int i = 0; i <n ; i++){
             for(int j = 0; j<m; j++){
                 row[i] = min(row[i], matrix[i][j]);
-            }
-        }
-
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j<n; j++){
-                col[i] = max(col[i], matrix[j][i]);
+                col[j] = max(col[j], matrix[i][j]);
             }
         }
 
