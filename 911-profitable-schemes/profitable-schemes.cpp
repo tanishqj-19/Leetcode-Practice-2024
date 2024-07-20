d5a48acaeab7dc51871be0309a8e9e873f29792a
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-07-20 17:19:07 +0530

    Time: 23 ms (98.36%) | Memory: 11 MB (92.35%) - LeetSync

--- 911-profitable-schemes/profitable-schemes.cpp
+++ 911-profitable-schemes/profitable-schemes.cpp
@@ -1,31 +1,28 @@
 class Solution {
     const int mod = 1e9 + 7;
     int sz;
-    int dp[101][101][101];
-    int solve(int i, int n, int minProfit, vector<int>& group, vector<int>& profit, int member){
-        if(minProfit <= 0)
-            minProfit = 0;
-        if(i == sz){
-            return minProfit <= 0 ? 1 : 0;
-        }
-
-        if(dp[i][member][minProfit] != -1)
-            return dp[i][member][minProfit];
-
-        int notPick = solve(i + 1, n, minProfit, group, profit, member);
-        int pick = 0;
-
-        if(group[i] + member <= n)
-            pick = solve(i + 1, n, minProfit - profit[i], group, profit, member + group[i]);
-        
-        return dp[i][member][minProfit] = (pick + notPick) % mod;
-
-        
-    }
+    
+    
 public:
     int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
         sz = group.size();
-        memset(dp, -1, sizeof(dp));
-        return solve(0, n, minProfit, group, profit, 0);
+        vector<vector<int>> dp(n + 1, vector<int>(minProfit + 1, 0));
+        dp[0][0] = 1;
+
+        for(int i = 0; i<sz; i++){
+            int grp = group[i], curr_p = profit[i];
+            for(int j = n; j>=grp; j--){
+                for(int p = minProfit; p >= 0; p--){
+                    dp[j][p] = (dp[j][p] + dp[j - grp][max(0, p - curr_p)]) % mod;
+                }
+            }
+        }
+
+        int totalSchemes = 0;
+        for (int j = 0; j <= n; ++j) {
+            totalSchemes = (totalSchemes + dp[j][minProfit]) % mod;
+        }
+
+        return totalSchemes;
     }
 };
