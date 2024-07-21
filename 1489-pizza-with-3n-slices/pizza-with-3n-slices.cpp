47e31bd1bdde165fe46639f915ce40bafc21915e
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-07-21 22:57:06 +0530

    Time: 16 ms (35.74%) | Memory: 16.9 MB (49.03%) - LeetSync

--- 1489-pizza-with-3n-slices/pizza-with-3n-slices.cpp
+++ 1489-pizza-with-3n-slices/pizza-with-3n-slices.cpp
@@ -1,28 +1,32 @@
 class Solution {
-    int solve(vector<int> &slices, int i, int numSlice, int end, vector<vector<int>> &dp){
-        if(numSlice == 0 || i > end)
-            return 0;
-        if(dp[i][numSlice] != -1)
-            return dp[i][numSlice];
-        
-        int pick = slices[i] + solve(slices, i + 2, numSlice-1, end, dp);
-        int notPick = solve(slices, i + 1, numSlice, end, dp);
-
-        return dp[i][numSlice] = max(pick, notPick);
-
-        
-    }
+    
 public:
     int maxSizeSlices(vector<int>& slices) {
         int n = slices.size();
 
-        vector<vector<int>> dp1(n , vector<int>(n, -1));
-        int first = solve(slices, 0, n/3, n-2, dp1);
-        vector<vector<int>> dp2(n, vector<int>(n, -1));
+        vector<vector<int>> dp1(n + 1 , vector<int>(n/3 + 1, 0));
 
-        int second  = solve(slices, 1, n/3, n-1, dp2);
+        for(int i = n-2; i>=0; i--){
+            for(int j = 1; j <= n/3; j++){
+                int notPick = dp1[i+1][j];
+                int pick = dp1[i+2][j-1] + slices[i]; 
 
-        return max(first, second);
+                dp1[i][j] = max(pick, notPick);
+            }
+        }
+        vector<vector<int>> dp2(n + 2, vector<int>(n/3 + 1, 0));
+
+        for(int i = n-1; i>=1; i--){
+            for(int j =1; j<= n/3; j++){
+                int notPick = dp2[i+1][j];
+                int pick = dp2[i+2][j-1] + slices[i]; 
+
+                dp2[i][j] = max(pick, notPick);
+            }
+        }
+
+        return max(dp1[0][n/3],dp2[1][n/3]);
+
 
     }
 };
