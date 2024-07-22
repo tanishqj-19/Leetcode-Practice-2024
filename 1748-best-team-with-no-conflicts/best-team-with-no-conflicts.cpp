00aa2e8191efb09382ebeef95851489f94ad55d7
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-07-22 15:15:55 +0530

    Time: 136 ms (42.00%) | Memory: 136.3 MB (29.94%) - LeetSync

--- 1748-best-team-with-no-conflicts/best-team-with-no-conflicts.cpp
+++ 1748-best-team-with-no-conflicts/best-team-with-no-conflicts.cpp
@@ -1,34 +1,34 @@
 class Solution {
 
     int n;
-    int dp[1001][1002];
+    
 
-    vector<pair<int,int>> arr;
+    
 
-    int bestScore(int i, int prev){
-        if(i >= n)
-            return 0;
-        if(dp[i][prev + 1]  != -1)
-            return dp[i][prev + 1];
+    // int bestScore(int i, int prev){
+    //     if(i >= n)
+    //         return 0;
+    //     if(dp[i][prev + 1]  != -1)
+    //         return dp[i][prev + 1];
 
-        int notPick = bestScore(i + 1, prev);
-        int pick = 0;
+    //     int notPick = bestScore(i + 1, prev);
+    //     int pick = 0;
 
-        if(prev != -1 && arr[prev].first > arr[i].first && arr[i].second > arr[prev].second){
-            return dp[i][prev + 1] = max(pick, notPick);
-        }
-        pick = arr[i].second + bestScore( i + 1, i);
+    //     if(prev != -1 && arr[prev].first > arr[i].first && arr[i].second > arr[prev].second){
+    //         return dp[i][prev + 1] = max(pick, notPick);
+    //     }
+    //     pick = arr[i].second + bestScore( i + 1, i);
         
 
-        return dp[i][prev + 1] = max(pick, notPick);
+    //     return dp[i][prev + 1] = max(pick, notPick);
         
-    }
+    // }
 public:
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
         n = scores.size();
 
-        memset(dp, -1, sizeof(dp));
-        arr.resize(n);
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        vector<pair<int,int>> arr(n);
 
         for(int i = 0; i<n; i++){
             arr[i] = {ages[i], scores[i]};
@@ -36,6 +36,20 @@ public:
 
         sort(arr.begin(), arr.end(), greater<pair<int, int>> ());
 
-        return bestScore( 0, -1);
+        for(int i = n-1; i>= 0; i--){
+            for(int prev = i-1; prev >= -1; prev--){
+                int notPick = dp[i + 1][prev + 1];
+
+                if(prev != -1 && arr[prev].first > arr[i].first && 
+                    arr[i].second > arr[prev].second){
+                    dp[i][prev + 1] = notPick;
+                }else{
+                    dp[i][prev + 1] = max(notPick, arr[i].second + dp[i + 1][i + 1]);
+                }
+
+            }
+        }
+
+        return dp[0][0];
     }
 };
