e8de79cdbe3e7b7fa659bcfe3818d48a68a50bc5
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-07-23 12:06:18 +0530

    Time: 7 ms (55.39%) | Memory: 15.1 MB (14.29%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -1,10 +1,5 @@
 class Solution {
-    static bool cmp(pair<int, int> &a, pair<int, int> &b){
-        if(a.second == b.second)
-            return a.first > b.first;
-        else
-            return a.second < b.second;
-    }
+    
 public:
     vector<int> frequencySort(vector<int>& nums) {
         
@@ -12,20 +7,12 @@ public:
 
         for(int &n: nums) count[n]++;
 
-        vector<pair<int, int>> vec;
+        sort(nums.begin(), nums.end(), [&](int a, int b){
+            return count[a] == count[b] ?  a > b : count[a] < count[b];
+        });
 
-        for(auto x: count){
-            vec.push_back({x.first, x.second});
-        }
-
-        sort(vec.begin(), vec.end(), cmp);
-        vector<int> ans;
-        for(int i=0; i<vec.size(); i++){
-            while(vec[i].second--> 0){
-                ans.push_back(vec[i].first);
-            }
-        }
-        return ans;
+       
+        return nums;
 
 
 
