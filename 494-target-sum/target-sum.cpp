a688964c71c893cc0bbb6798a94014a73506ad18
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-07-25 12:38:58 +0530

    Time: 19 ms (45.93%) | Memory: 41.9 MB (18.94%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,24 +1,23 @@
 class Solution {
-    int solve(int i, vector<int> &nums, int x, int target,vector<vector<int>> &dp){
-        if(i < 0 && x == target)
-            return 1;
-        if(i < 0)
-            return 0;
-        if(dp[i][x + 1000]  != -1)
-            return dp[i][x + 1000];
-        
-        
-        
-        int neg =  solve(i -1, nums, x - nums[i], target, dp);
-        int pos = solve(i -1, nums, x + nums[i], target, dp);
-
-        return dp[i][x + 1000] =  neg + pos;
-    }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(2002, -1));
-        return solve(n-1, nums, 0, target, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(2001, 0));
+        
+        // Base case: There's one way to achieve sum 0 with no elements (by doing nothing)
+        dp[0][1000] = 1;
+        
+        // Fill the DP table
+        for (int i = 1; i <= n; ++i) {
+            for (int sum = -1000; sum <= 1000; ++sum) {
+                if (dp[i-1][sum + 1000] != 0) {
+                    dp[i][sum + nums[i-1] + 1000] += dp[i-1][sum + 1000];
+                    dp[i][sum - nums[i-1] + 1000] += dp[i-1][sum + 1000];
+                }
+            }
+        }
+        
+        // The result is the number of ways to get to the target sum
+        return target > 1000 || target < -1000 ? 0 : dp[n][target + 1000];
     }
 };
