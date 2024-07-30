bba6c768c1f8b34f049a1c446b739d15f8e59b06
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-07-30 16:13:25 +0530

    Time: 6 ms (51.34%) | Memory: 8.6 MB (54.65%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,45 +1,35 @@
 class Solution {
 
-   int n1,n2,n3;
-   int dp[101][101][201];
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        n3 = s3.size(), n2 = s2.size(), n1 = s1.size();
+        int n3 = s3.size(), n2 = s2.size(), n1 = s1.size();
 
         if(n2 + n1 != n3)
             return false;
-        
         if(n1 == 0 && n2 == 0 && n3 == 0)
             return true;
-        memset(dp, -1,  sizeof(dp));
+        vector<vector<bool>> dp(n1 + 1, vector<bool>(n2 + 1, false));
+        dp[0][0] = 1;
 
-        return f(s1, s2, s3, 0, 0, 0);
+        for(int i = 0; i<=n1; i++){
+            for(int j = 0; j<=n2; j++){
+                bool sol = false;
+                int idx = i + j-1;
+                if(i > 0){
+                    if(s1[i-1] == s3[idx])
+                        dp[i][j] = dp[i][j] || dp[i-1][j];
+                }
+
+                if(j > 0){
+                    if(s2[j-1] == s3[idx])
+                        dp[i][j] = dp[i][j] || dp[i][j-1];
+                }
+
+            }
+        }
+
+        return dp[n1][n2];
     }
 
-    bool f(string &s1, string &s2, string &s3, int i,int j,int k){
-        if(i >= n1 && j >= n2 && k >= n3)
-            return true;
-        if(s1[i] != s3[k] && s3[k] != s2[j])
-            return false;
-        if(i >= n1){
-            if(s2[j] != s3[k])
-                return false;
-        }
-
-        if(j >= n2){
-            if(s1[i] != s3[k])
-                return false;
-        }
-
-        if(dp[i][j][k] != -1)
-            return dp[i][j][k];
-        bool sol = false;
-        if(s1[i] == s3[k]){
-            sol |= f(s1, s2, s3, i + 1, j, k + 1);
-        }
-
-        if(s2[j] == s3[k])
-            sol |= f(s1, s2, s3, i, j + 1, k + 1);
-        return dp[i][j][k] =  sol;
-    }
+    
 };
