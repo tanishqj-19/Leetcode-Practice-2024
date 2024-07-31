0a10e710863a0270f0e9eecfae92622e81c7636d
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-08-01 01:47:56 +0530

    Time: 354 ms (44.29%) | Memory: 85.2 MB (33.21%) - LeetSync

---
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -0,0 +1,21 @@
+#define ll long long
+
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        unordered_map<int, int> freq;
+
+        int n = nums.size();
+
+        int ans = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j <i; j++){
+                ll pro = (ll) nums[i] *(ll) nums[j];
+                ans += 8 * freq[pro];
+                freq[pro]++;
+            }
+        }
+
+        return ans;
+    }
+};
