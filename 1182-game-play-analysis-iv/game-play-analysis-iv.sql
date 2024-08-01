85bc7591856fcb7fbd282390c944de601bd131ac
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-08-02 01:29:30 +0530

    Time: 541 ms (66.62%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,17 +1,17 @@
 # Write your MySQL query statement below
 
--- SELECT ROUND( COUNT(distinct player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) as fraction
--- FROM Activity
--- WHERE (player_id, first_login) IN (
---     SELECT player_id, min(event_date) as first_login
---     FROM  Activity
---     GROUP BY player_id
--- )
-
-SELECT ROUND(SUM(IF(Activity.event_date = DATE_ADD(min_date, INTERVAL 1 DAY), 1, 0)) / count(distinct Activity.player_id), 2) as fraction
+SELECT ROUND( COUNT(distinct player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) as fraction
 FROM Activity
-JOIN (   SELECT player_id, min(event_date) as min_date
-    FROM Activity 
+WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY)) IN (
+    SELECT player_id, min(event_date) as first_login
+    FROM  Activity
     GROUP BY player_id
-) as sub
-WHERE Activity.player_id = sub.player_id
+)
+
+-- SELECT ROUND(SUM(IF(Activity.event_date = DATE_ADD(min_date, INTERVAL 1 DAY), 1, 0)) / count(distinct Activity.player_id), 2) as fraction
+-- FROM Activity
+-- JOIN (   SELECT player_id, min(event_date) as min_date
+--     FROM Activity 
+--     GROUP BY player_id
+-- ) as sub
+-- WHERE Activity.player_id = sub.player_id
