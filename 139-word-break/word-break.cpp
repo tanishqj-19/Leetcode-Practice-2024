d93d9fbced11c32bbfb797a886bda9d3ce54522e
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-08-03 23:57:28 +0530

    Time: 5 ms (74.29%) | Memory: 10.1 MB (91.00%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,29 +1,29 @@
 class Solution {
-    unordered_set<string> st;
-    int dp[301];
-    bool check(string &s, int i, int n){
-        if(i >= n){
-            return true;
-        }
-        if(dp[i] != -1)
-            return dp[i];
-        for(int idx = 1; i + idx <= n; idx++){
-            string temp = s.substr(i, idx);
-            if(st.find(temp) != st.end()){
-                bool next = check(s, i + idx, n);
-                if(next)
-                    return dp[i] = true;
-            }
-        }
-        return dp[i] = false;
-    }
+    
+    bool dp[301];
+    
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
-        memset(dp, - 1, sizeof(dp));
-        for(string &c: wordDict){
-            st.insert(c);
+        memset(dp, 0, sizeof(dp));
+        
+
+        for(int i =0; i<n; i++){
+            for(auto &word: wordDict){
+                int sz = word.size();
+                if(i < sz - 1)
+                    continue;
+
+                if(i == sz - 1 || dp[i - sz]){
+                    if(s.substr(i -sz  + 1, sz) == word){
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
         }
-        return check(s,0,n);
+
+
+        return dp[n-1];
     }
 };
