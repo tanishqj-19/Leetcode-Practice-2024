3826039cb124653a28aa7620f8a6008d178f3a50
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-08-05 12:02:47 +0530

    Time: 1128 ms (18.26%) | Memory: 263.4 MB (5.76%) - LeetSync

--- 1825-find-minimum-time-to-finish-all-jobs/find-minimum-time-to-finish-all-jobs.cpp
+++ 1825-find-minimum-time-to-finish-all-jobs/find-minimum-time-to-finish-all-jobs.cpp
@@ -1,11 +1,12 @@
 #define ll long long
 
 class Solution {
-    int n;
+    int n, k;
     ll maxWork;
+    vector<ll> work;
     
 
-    void fun(vector<int> &jobs, int &k, int idx, vector<ll> & work){
+    void fun(vector<int> &jobs, int idx){
         if(idx == n){
             ll load = *max_element(work.begin(), work.end());
             maxWork = min(maxWork, load);
@@ -19,7 +20,7 @@ class Solution {
 
             s.insert(work[i]);
             work[i] += jobs[idx];
-            fun(jobs, k, idx + 1, work);
+            fun(jobs, idx + 1);
             work[i] -= jobs[idx];
         }
 
@@ -29,13 +30,11 @@ class Solution {
 public:
     int minimumTimeRequired(vector<int>& jobs, int k) {
         n = jobs.size();
+        this->k = k;
         maxWork = LLONG_MAX;
-        if(n == k)
-            return *max_element(jobs.begin(), jobs.end());
         
-        vector<ll> work(k, 0);
-        
-        fun(jobs, k, 0, work);
+        work.resize(k, 0);        
+        fun(jobs, 0);
         return maxWork;
     }
 };
