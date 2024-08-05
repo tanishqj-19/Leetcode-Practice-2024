8346589e35a0be4ea92bc9422eb43d517f782d0f
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-08-05 09:46:55 +0530

    Time: 16 ms (59.81%) | Memory: 18.5 MB (31.70%) - LeetSync

--- 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.cpp
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.cpp
@@ -3,25 +3,14 @@ public:
     string kthDistinct(vector<string>& arr, int k) {
         int n = arr.size();
 
-        if(n < k)
-            return "";
-        map<int, string> track;
-        unordered_map<string, int > vis;
+        unordered_map<string, int > track;
         for(int i = 0; i<n;i++){
-            if(vis.find(arr[i]) == vis.end()){
-                track[i] = arr[i];
-                vis[arr[i]] = 1;
-            }else{
-                vis[arr[i]] = 2;
-            }
+            track[arr[i]]++;
         }
 
-        for(auto &x: track){
-            
-            if(vis[x.second] == 1)
-                k--;
-            if(k == 0)
-                return x.second;
+        for(auto &x: arr){
+            if(track[x] == 1 && --k == 0)
+                return x;
         }
 
         return "";
