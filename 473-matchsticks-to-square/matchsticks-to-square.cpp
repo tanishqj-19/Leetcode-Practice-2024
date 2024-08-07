0f45b8fe67a700ad13c1ddf2494d1614d939abe4
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-08-07 14:34:18 +0530

    Time: 592 ms (11.20%) | Memory: 12.9 MB (19.76%) - LeetSync

---
+++ 473-matchsticks-to-square/matchsticks-to-square.cpp
@@ -0,0 +1,52 @@
+#define ll long long
+
+class Solution {
+    vector<bool> vis;
+
+    int tar;
+
+    bool subsetSum(vector<int> &arr, int idx, int k, ll sum){
+        if(k == 1)
+            return true;
+        if(sum > tar)
+            return false;
+        if(sum == tar){
+            
+            return subsetSum(arr, 0, k-1, 0);
+        }
+        
+        for(int i = idx; i <arr.size(); i++){
+            if(!vis[i] && sum + (ll) arr[i] <= tar){
+                vis[i] = true;
+                if(subsetSum(arr, i + 1, k, sum + arr[i]))
+                    return true;
+                vis[i] = false;
+
+
+            }
+        }
+
+        return false;
+    }
+public:
+    bool makesquare(vector<int>& matchsticks) {
+        int n = matchsticks.size();
+
+        ll sum = accumulate(matchsticks.begin(), matchsticks.end(), 0); 
+
+        if(n < 4 || sum % 4)
+            return false;
+        sort(matchsticks.begin(), matchsticks.end(), greater<int>());
+        int k = sum / 4;
+        tar = k;
+        vis.resize(n, false);
+    
+        return subsetSum(matchsticks, 0, 4, 0);
+
+    
+
+
+
+
+    }
+};
