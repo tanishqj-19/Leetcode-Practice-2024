0de9a038cc8be19c2dfd9c7e26aa3275e8e4dba0
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-08-13 23:11:17 +0530

    Time: 173 ms (72.98%) | Memory: 156.9 MB (16.24%) - LeetSync

--- 3106-length-of-the-longest-subsequence-that-sums-to-target/length-of-the-longest-subsequence-that-sums-to-target.cpp
+++ 3106-length-of-the-longest-subsequence-that-sums-to-target/length-of-the-longest-subsequence-that-sums-to-target.cpp
@@ -1,26 +1,29 @@
 class Solution {
-    int dp[1001][1001];
-    int f(vector<int> &nums, int k, int i){
-        if(k == 0)
-            return 0;
-        
-        if(k < 0 || i >= nums.size())
-            return INT_MIN;
-        if(dp[i][k] != -1)
-            return dp[i][k];
-        
-        
-
-        int notPick = f(nums, k, i + 1);
-        int pick = 1 + f(nums, k - nums[i], i + 1);
-        
-
-        return dp[i][k] = max(pick, notPick);
-    }
+    
+    
 public:
     int lengthOfLongestSubsequence(vector<int>& nums, int target) {
-        memset(dp, -1, sizeof(dp));
-        int ans = f(nums, target,  0);
-        return ans < 0? -1: ans;
+        int n = nums.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1));
+        
+
+        for(int i = 0; i<=n; i++){
+            dp[i][0] = 0;
+        }
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= target; j++){
+                int notPick = dp[i-1][j];
+            
+                int pick = -1;
+
+                if(j >= nums[i-1] && dp[i - 1][j - nums[i - 1]] != -1)
+                    pick = 1 + dp[i-1][j-nums[i-1]];
+
+                dp[i][j] = max(pick, notPick);
+            }
+        }
+
+        return dp[n][target];
     }
 };
