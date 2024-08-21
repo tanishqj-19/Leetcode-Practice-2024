7c30df90c1712059ccddf1e146f7a29083013ff8
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-08-21 18:14:39 +0530

    Time: 18 ms (41.56%) | Memory: 13.3 MB (6.01%) - LeetSync

---
+++ 16-3sum-closest/3sum-closest.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+
+        std::sort(nums.begin(), nums.end());
+        int Finalsum = nums[0] + nums[1] + nums[2];
+        for(int i=0; i<nums.size()-2; i++){
+            int front = i+1, back = nums.size() - 1;
+
+            while(front < back){
+                int sum = nums[i] + nums[front] + nums[back];
+                
+                if(sum == target )
+                    return sum;
+                else if (abs(target - sum) < abs(target-Finalsum))
+                    Finalsum = sum;
+                
+                if (sum > target)
+                    back--;
+                else 
+                    front++;
+            }
+        }
+        return Finalsum;
+        
+    }
+};
