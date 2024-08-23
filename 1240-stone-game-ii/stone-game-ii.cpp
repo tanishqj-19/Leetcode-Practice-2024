54123b3825b310154256caaba4013385c67857a1
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-08-23 12:16:59 +0530

    Time: 14 ms (45.14%) | Memory: 10.4 MB (93.70%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -1,36 +1,42 @@
 class Solution {
     int n;
-    int dp[101][101];
-    int f(vector<int> &piles, int i, int M, int total){
-        if(i >= n)
-            return 0;
-        if(dp[i][M] != -1)
-            return dp[i][M];
+    int dp[101][101][2]; 
+    
+    int f(vector<int>& piles, int i, int M, bool aliceTurn) {
+        if (i >= n) return 0; 
         
+        if (dp[i][M][aliceTurn] != -1) return dp[i][M][aliceTurn]; 
         
         int m = min(2 * M + i, n);
-        int sum = 0, taken = 0;
-        for(int x = i ; x < m; x++){
-            taken += piles[x];
-            int newM = max(M, x - i + 1);
-
-            sum = max(sum, total  - f(piles, x + 1, newM, total - taken));
+        int result;
+        int taken = 0;
+        
+        if (aliceTurn) {
+            // Alice's turn, maximize her score.
+            result = 0;
+            for (int x = i; x < m; x++) {
+                taken += piles[x];
+                int newM = max(M, x - i + 1);
+                result = max(result, taken + f(piles, x + 1, newM, false)); 
+            }
+        } else {
+            
+            result = INT_MAX;
+            for (int x = i; x < m; x++) {
+                taken += piles[x];
+                int newM = max(M, x - i + 1);
+                result = min(result, f(piles, x + 1, newM, true)); 
+            }
         }
 
-        return dp[i][M] = sum;    
+        return dp[i][M][aliceTurn] = result;
     }
+    
 public:
     int stoneGameII(vector<int>& piles) {
         n = piles.size();
-
         memset(dp, -1, sizeof(dp));
 
-
-        int total = accumulate(piles.begin(), piles.end(), 0);
-
-        return f(piles, 0, 1, total);
-
-
-
+        return f(piles, 0, 1, true);
     }
 };
