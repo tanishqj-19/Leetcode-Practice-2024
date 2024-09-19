9b6db0462e8e20a98c6617b17b72b15f7db1fc11
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-09-19 21:14:41 +0530

    Time: 40 ms (95.72%) | Memory: 38.3 MB (86.14%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -5,38 +5,18 @@ public:
 
         for(char &c: tasks) freq[c - 'A']++;
 
-        priority_queue<int> pq;
+        sort(freq.begin(), freq.end());
 
-        for(int &i: freq){
-            if(i > 0)
-                pq.push(i);
+        int maxi = freq[25] - 1;
+        int task = tasks.size();
+
+        int idle = maxi * n;
+
+        for(int i = 24; i >= 0 && freq[i] > 0 ; i--){
+            idle -= min(maxi, freq[i]);
         }
-
-
-        int least = 0, cycle, task;
-        while(!pq.empty()){
-            vector<int> curr;
-            cycle = n + 1;
-            task = 0;
-            while(cycle-- && !pq.empty()){
-                if(pq.top() > 1)
-                    curr.push_back(pq.top() - 1);
-                pq.pop();
-                task++;
-            }
-            
-            for(auto &x:  curr)
-                pq.push(x);
-            
-
-            if(!pq.empty())
-                least += (n + 1);
-            else
-                least += task;
-
-        }
-
-        return least;
+    
+        return idle > 0 ? task + idle : task;
 
 
          
