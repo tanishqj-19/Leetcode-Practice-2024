09f8195e07b213e3e15896fa74b8232e0ed14304
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-10-11 11:50:09 +0530

    Time: 141 ms (71.48%) | Memory: 57.1 MB (88.66%) - LeetSync

---
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        int n = times.size();
+        int k = times[targetFriend][0];
+        sort(times.begin(), times.end());
+
+        int maxEnd = 0;
+
+        priority_queue<int, vector<int>, greater<int>> time;
+
+        for(int i =0; i<n; i++) time.push(i);
+
+        priority_queue<pair<int, int>, vector<pair<int, int>> , greater<pair<int, int>>> pq;
+
+        for(auto &a: times){
+            int arrival = a[0], departure = a[1];
+
+            while(!pq.empty() && pq.top().first <= arrival){
+                time.push(pq.top().second); pq.pop();
+            }
+
+            int currChair;
+
+            if(!time.empty()){
+                currChair = time.top(); time.pop();
+            }else{
+                currChair = pq.top().second;
+            }
+
+            pq.push({a[1], currChair});
+            if(a[0] == k){
+                return currChair;
+            }
+        }
+        return 0;
+
+
+
+
+
+    }
+};
