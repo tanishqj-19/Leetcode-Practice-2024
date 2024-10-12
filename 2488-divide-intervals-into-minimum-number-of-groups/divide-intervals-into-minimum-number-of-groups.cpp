e8bb90a184927e34c823ae945c79971c9ea9befe
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-10-12 19:48:02 +0530

    Time: 236 ms (90.77%) | Memory: 214.6 MB (14.41%) - LeetSync

--- 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.cpp
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.cpp
@@ -2,19 +2,25 @@ class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
 
-        sort(intervals.begin(), intervals.end());
-
-        priority_queue<int, vector<int>, greater<int>> pq;
-
-
-        for(auto & interval: intervals){
-            if(!pq.empty() && pq.top() < interval[0]){
-                pq.pop();
-            }
-            pq.push(interval[1]);
-
+        int l =INT_MAX, r = 0;
+        for(auto &x: intervals){
+            l = min(l, x[0]);
+            r = max(r, x[1]);
         }
 
-        return pq.size();
+        vector<int> lineSweep(r + 2, 0);
+
+        for(auto &x: intervals){
+            lineSweep[x[0]]++;
+            lineSweep[x[1] + 1]--;
+        }
+
+        int minGroups = 0, currGroup = 0;
+
+        for(int i = l; i <= r + 1; i++){
+            currGroup += lineSweep[i];
+            minGroups = max(minGroups, currGroup);
+        }
+        return minGroups;
     }
 };
