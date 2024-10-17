1a212b8429d33623d8d12e9a20ee17934a5ebd6b
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-10-17 09:45:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (70.34%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -3,16 +3,21 @@ public:
     int maximumSwap(int num) {
         string s  = to_string(num);
 
-        int n  = s.size();
-        
+        int  n  = s.size();
+
+        vector<int> maxRight(n);
+
+        maxRight[n-1] = n-1;
+
+        for(int i = n-2; i>=0; i--){
+            maxRight[i] = (s[i] > s[maxRight[i + 1]] ? i : maxRight[i + 1]);
+        }
+
         for(int i = 0; i<n; i++){
-            
-            for(int j = i + 1; j<n; j++){
-                swap(s[i], s[j]);
+            if(s[i] < s[maxRight[i]]){
+                swap(s[i], s[maxRight[i]]);
 
-                num = max(num, stoi(s));
-                swap(s[i], s[j]);
-
+                return stoi(s);
             }
         }
 
