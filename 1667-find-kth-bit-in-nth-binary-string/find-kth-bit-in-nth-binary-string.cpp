014aa8711489d3689fd9734d9dafd00016abfd99
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-10-19 10:42:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (87.54%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -1,38 +1,21 @@
 class Solution {
-
-    string invertReversed(string s){
-        for(char &c: s){
-            if(c == '0'){
-                c = '1';
-            }else{
-                c = '0';
-            }
-        }
-        reverse(s.begin(), s.end());
-        return s;
-    }
-
 public:
     char findKthBit(int n, int k) {
+        if(n == 1)
+            return '0';
         
-        vector<string> dp(n + 1);
+        int size = 1 << n;
 
-        dp[1] = "0"; 
+        if(k < size/2){
+            return findKthBit(n-1, k);
+        }else if(k == size / 2){
+            return '1';
+        }else{
+            char temp = findKthBit(n-1, size - k);
 
-        if(n == 1){
-            return dp[1][k-1]; 
+            return (temp == '0') ? '1' : '0';
+
+
         }
-        dp[2] = "011";
-        if(n == 2)
-            return  dp[2][k-1];
-
-        for(int i = 3; i<= n; i++){
-            string temp = invertReversed(dp[i-1]);
-            dp[i] = (dp[i-1] + "1" + temp);
-        }
-    
-
-        return dp[n][k-1];
-        
     }
 };
