e004fe6ed24d44f685d0f52b77e6a482eb92d54d
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-11-04 17:38:26 +0530

    Time: 35 ms (59.83%) | Memory: 29.9 MB (24.06%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.cpp
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -2,30 +2,25 @@ class Solution {
 public:
     string compressedString(string word) {
         int n = word.size();
-        if(n == 1){
-            string ans = "1";
-            ans += word.back();
-            return ans;
-        }
+        
 
         string comp = "";
 
         int cnt = 1;
-        char curr = word[0];
+        // char curr = word[0];
 
         for(int i = 1; i<n; i++){
             if(word[i] == word[i-1] && cnt < 9){
                 cnt++;
             }else{
                 comp += to_string(cnt);
-                comp += curr;
-                curr = word[i];
+                comp += word[i-1];
                 cnt = 1;
             }
         }
 
         comp += to_string(cnt);
-        comp += curr;
+        comp += word.back();
 
         return comp;
     }
