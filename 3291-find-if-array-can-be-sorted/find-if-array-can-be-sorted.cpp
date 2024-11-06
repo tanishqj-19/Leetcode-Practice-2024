97f066393e24a8a6c431a53f1c924ab39f2682a8
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-11-06 17:27:06 +0530

    Time: 4 ms (34.53%) | Memory: 31.1 MB (94.95%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -5,23 +5,21 @@ public:
     bool canSortArray(vector<int>& nums) {
         int n = nums.size();
 
-
         for(int i = 0; i <n; i++){
-            int minIdx = i;
             int minVal, currVal;
-            for(int j = i + 1; j<n; j++){
-                if(nums[j] < nums[minIdx]){
-                    minIdx = j;
-                    break;
+            for(int j = 0; j<n-i-1; j++){
+                if(nums[j] <= nums[j + 1]){
+                    continue;
+                }else{
+                    minVal = __builtin_popcount(nums[j + 1]);
+                    currVal = __builtin_popcount(nums[j]);
+
+                    if(minVal != currVal)
+                        return false;
+                    swap(nums[j], nums[j + 1]);
                 }
             }
 
-            minVal = __builtin_popcount(nums[minIdx]);
-            currVal = __builtin_popcount(nums[i]);
-
-            if(currVal != minVal)
-                return false;
-            swap(nums[minIdx], nums[i]);
         }
         return true;
     }
