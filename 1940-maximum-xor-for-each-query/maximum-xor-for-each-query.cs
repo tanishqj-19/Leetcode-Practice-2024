08c552478581f77da2d1b978f1362ed20b8b8d71
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-11-08 21:43:44 +0530

    Time: 2 ms (100.00%) | Memory: 108.2 MB (9.09%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.cs
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.cs
@@ -1,19 +1,19 @@
 public class Solution {
     public int[] GetMaximumXor(int[] nums, int maximumBit) {
         int n = nums.Length;
-        int[] prefix_xor = new int[n];
-
-        prefix_xor[0] = nums[0];
+        int xor = 0;
         int[] res = new int[n];
 
         int maxVal = (1 << maximumBit) - 1;
-
-        for(int i = 1; i<n; i++){
-            prefix_xor[i] = nums[i] ^ prefix_xor[i-1];
+        foreach(int num in nums){
+            xor ^= num;
         }
+        
+        
 
         for(int i = 0; i <n; i++){
-            res[i] = prefix_xor[n-i-1] ^ maxVal;
+            res[i] = xor ^ maxVal;
+            xor ^= nums[n-i-1];
         }
 
         return res;
