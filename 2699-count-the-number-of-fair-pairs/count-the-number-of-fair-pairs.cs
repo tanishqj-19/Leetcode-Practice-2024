4329199b271f4f27c1ce833207256e37ecf8a68b
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-11-13 22:38:17 +0530

    Time: 22 ms (100.00%) | Memory: 67.2 MB (83.33%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cs
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cs
@@ -1,32 +1,25 @@
 public class Solution {
 
-    private long binarySearch(int[] nums,int l, int r, int k){
+    private long helper(int[] nums, int k){
         
-        while( l <= r){
-            int mid = l + (r - l) / 2;
+        int i = 0, j = nums.Length - 1;
+        long count = 0;
+        while(i < j){
+            int sum = nums[i] + nums[j];
 
-            if(nums[mid] < k){
-                l = mid + 1;
+            if(sum < k){
+                count += (j - i);
+                i++;
             }else{
-                r = mid-1;
+                j--;
             }
         }
 
-        return l;
+        return count;
     }
     public long CountFairPairs(int[] nums, int lower, int upper) {
         Array.Sort(nums);
-        int n = nums.Length;
-        long cnt=0;
-        for(int i = 0; i<n; i++){
-
-            long lowerLimit = binarySearch(nums, i + 1, n-1,  lower - nums[i]);
-            long upperLimit = binarySearch(nums, i + 1, n-1, upper + 1 -nums[i]);
-
-            cnt += Math.Abs(lowerLimit - upperLimit);
-
-        }
-
-        return cnt;
+        
+        return helper(nums, upper + 1) - helper(nums, lower);
     }
 }
