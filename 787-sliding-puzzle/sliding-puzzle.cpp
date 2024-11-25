93670029f706520579482be40a036ab0b0fae933
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-11-25 10:57:07 +0530

    Time: 4 ms (78.31%) | Memory: 12.3 MB (63.55%) - LeetSync

---
+++ 787-sliding-puzzle/sliding-puzzle.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int slidingPuzzle(vector<vector<int>>& board) {
+        string state = "", k = "123450";
+        int src= 0;
+        for(int i = 0; i<2; i++){
+            for(int j = 0; j < 3; j++){
+                state += (board[i][j] + '0');
+                if(state.back() == '0'){
+                    src = state.size() - 1;
+                }
+            }
+        }
+
+
+        vector<vector<int>> neighbors = {
+            {1, 3},       
+            {0, 2, 4},    
+            {1, 5},       
+            {0, 4},       
+            {1, 3, 5},    
+            {2, 4}        
+        };
+
+        queue<pair<string, int>> q; 
+        q.push({state, 0});
+
+        
+        unordered_set<string> visited;
+        visited.insert(state);
+        while(!q.empty()){
+            auto [curr, moves] =  q.front(); q.pop();
+
+            if(curr == k){
+                return moves;
+            }
+            int idx = curr.find('0');
+
+            for(auto &nei : neighbors[idx]){
+                string next = curr;
+                swap(next[idx], next[nei]);
+                if(!visited.count(next)){
+                    q.push({next, moves + 1});
+                    visited.insert(next);
+                }
+            }
+        }
+
+
+        return -1;
+
+    }
+};
