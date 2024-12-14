848688550bbac038e6ad56137a883ea18f8d2c5f
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-12-14 23:32:05 +0530

    Time: 106 ms (64.97%) | Memory: 113.1 MB (79.83%) - LeetSync

---
+++ 2868-continuous-subarrays/continuous-subarrays.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+        int i = 0, j = 0, n = nums.size();
+
+        long long count = 0;
+        map<int, int> freq;
+        while(j < n){
+            freq[nums[j]]++;
+            
+            while(i < n && freq.rbegin()->first - freq.begin()-> first > 2){
+                freq[nums[i]]--;
+                if(freq[nums[i]] == 0){
+                    freq.erase(nums[i]);
+                }
+                i++;
+            }
+            
+            count += (j - i + 1);
+            
+            j++;
+        }
+
+        return count;
+    }
+};
