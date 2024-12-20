2520a3370fbb2a42ca2a2fc2f4ddd19fb41cda18
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2024-12-20 20:48:44 +0530

    Time: 0 ms (100.00%) | Memory: 77 MB (77.24%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
@@ -1,41 +1,29 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+    void dfs(TreeNode* leftBaccha, TreeNode* rightBaccha, int level){
+        if(!leftBaccha)
+            return;
+        if(level % 2 == 0){
+            auto temp = rightBaccha->val;
+            rightBaccha->val = leftBaccha->val;
+            leftBaccha->val = temp;
+        }
+        dfs(leftBaccha->left, rightBaccha->right, level + 1);
+        dfs(leftBaccha->right, rightBaccha->left, level + 1);
+    }
 public:
     TreeNode* reverseOddLevels(TreeNode* root) {
-        queue<TreeNode*> q;
-
-        q.push(root);
-        int lv = 0;
-        while(!q.empty()){
-            int n = q.size();
-            vector<TreeNode*> parent;
-            vector<int> temp;
-            while(n-->0){
-                TreeNode* curr = q.front(); q.pop();
-
-                if(lv %  2 == 0){
-                    parent.push_back(curr);
-                }
-
-                if(curr->left){
-                     temp.push_back(curr->left->val);
-                    q.push(curr->left);
-                }
-
-                if(curr->right){
-                     temp.push_back(curr->right->val);
-                    q.push(curr->right);
-                }
-            }
-
-            if(!temp.empty())
-                for(auto node: parent){
-                    node->left->val = temp.back(); temp.pop_back();
-                    node->right->val = temp.back(); temp.pop_back();
-                }  
-
-            lv++;
-        }
+        dfs(root->left, root->right, 0);
 
         return root;
     }
